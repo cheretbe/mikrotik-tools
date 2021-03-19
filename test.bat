c1ceca31ddb0e883aabf3e41e33ff8f779d27d1a
cheretbe <a.a.orlov@gmail.com>
2021-03-19 11:12:21 +0200

    Move Windows python directory to lib

--- test.bat
+++ test.bat
@@ -6,6 +6,6 @@ IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
 SETLOCAL
 
 IF "%ProgramFiles(x86)%"=="" (SET CPUArch=x86) ELSE (SET CPUArch=x64)
-"%~dp0python\%CPUArch%\python.exe" "%~dp0lib\download_config.py" %*
+"%~dp0lib\python\%CPUArch%\python.exe" "%~dp0lib\download_config.py" %*
 
 ENDLOCAL
