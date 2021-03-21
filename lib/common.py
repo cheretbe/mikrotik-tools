1cc8f60b7dbd52566e0390dbe830e4c24753126f
cheretbe <a.a.orlov@gmail.com>
2021-03-21 22:30:53 +0200

    Finish config download script (Windows version)

---
+++ lib/common.py
@@ -0,0 +1,93 @@
+import sys
+import os
+import datetime
+import pathlib
+import paramiko
+import humanfriendly.prompts
+if sys.platform == 'win32':
+    import ctypes.wintypes
+
+def ssh_connect(ssh_host, credentials):
+    ssh_client=paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(
+        hostname=ssh_host, username=credentials["username"], password=credentials["password"]
+    )
+    return ssh_client
+
+
+def exec_ssh_command(ssh_client, cmd, echo=True):
+    result = []
+    stdin, stdout, stderr = ssh_client.exec_command(cmd)
+    stdin.close()
+    while True:
+        line = stdout.readline()
+        if not line:
+            break
+        result += [line.rstrip()]
+        if echo:
+            print(line, end="")
+    if stdout.channel.recv_exit_status() != 0:
+        raise Exception(
+            "SSH command returned non-zero exit status " +
+            str(stdout.channel.recv_exit_status())
+        )
+    return result
+
+def unique_file(file_names, template, extension):
+    c_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    file_counter = 0
+    while True:
+        file_name = template.format(c_date) + "_" + str(file_counter) + extension
+        if file_name not in file_names:
+            break
+        file_counter += 1
+    return file_name
+
+def get_backup_file_names(ssh_client):
+    file_names = []
+    for line in exec_ssh_command(ssh_client, "/file print terse", echo=False):
+        if line.strip():
+            file_names += [line.strip().split(" ")[1].split("=")[1]]
+    # file_names += ["flash"]
+    file_template="flash/{}" if "flash" in file_names else "{}"
+
+    return [
+        unique_file(file_names, file_template, ".backup"),
+        unique_file(file_names, file_template, ".rsc")
+    ]
+
+def get_documents_path():
+    if sys.platform == 'win32':
+        CSIDL_PERSONAL = 5      # My Documents
+        SHGFP_TYPE_CURRENT = 0  # Get current, not default value
+
+        buf = ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
+        ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_PERSONAL, None, SHGFP_TYPE_CURRENT, buf)
+        return(buf.value)
+    else:
+        return os.path.expanduser('~/Documents')
+
+def get_config_file_path():
+    if sys.platform == 'win32':
+        config_dir = os.path.expandvars(r"%APPDATA%\mikrotik-tools")
+    else:
+        config_dir = os.path.expanduser("~/.config/mikrotik-tools")
+    os.makedirs(config_dir, exist_ok=True)
+
+    return str(pathlib.Path(config_dir) / "settings.json")
+
+def select_host(recent_hosts):
+    selection = "Enter host name"
+    if len(recent_hosts) > 0:
+        selection = humanfriendly.prompts.prompt_for_choice(
+            ["Enter host name"] + recent_hosts + ["Exit"],
+            default="Enter host name"
+        )
+    if selection == "Exit":
+        sys.exit("Cancelled by user")
+    if selection == "Enter host name":
+        selection = humanfriendly.prompts.prompt_for_input("Enter host name: ")
+        if not selection:
+            sys.exit("ERROR: no host name has been supplied")
+    return selection
--- lib/download_config.py
+++ lib/download_config.py
@@ -1,22 +1,101 @@
-# No shebang as this script is intended to be run via bash or batch wrapper
+# No shebang as this script is intended to be run via bash or batch wrapper only
 
 import os
 import sys
+import pathlib
 import base64
 import json
+import argparse
+import datetime
 import colorama
+sys.path.append(os.path.dirname(__file__))
+import common
 
 
 colorama.init()
 
-print(colorama.Fore.RED + 'some red text')
 
-print(colorama.Fore.CYAN + 'cyan ' + colorama.Style.BRIGHT + 'bright ' + colorama.Style.NORMAL + 'test')
-print(colorama.Fore.MAGENTA + 'magenta ' + colorama.Style.BRIGHT + 'bright ' + colorama.Style.NORMAL + 'test')
+def main():
+    parser = argparse.ArgumentParser(description="Config backup utility for Mikrotik routers")
+    parser.add_argument(
+        "hosts", metavar="host_name", nargs="*", default="", help="Router name or IP address"
+    )
+    options = parser.parse_args()
 
-print(colorama.Style.RESET_ALL)
+    credentials = json.loads(base64.b64decode(os.environ["AO_MIKROTIK_CREDENTIALS"]).decode("utf-16"))
 
-credentials = json.loads(base64.b64decode(os.environ["AO_MIKROTIK_CREDENTIALS"]).decode("utf-16"))
-print(credentials)
+    dest_path = (
+        pathlib.Path(common.get_documents_path()) / "mikrotik-tools" /
+        datetime.datetime.now().strftime("%Y-%m-%d")
+    )
 
-sys.exit(0)
+    config_file_name = common.get_config_file_path()
+    if os.path.isfile(config_file_name):
+        with open(config_file_name) as conf_f:
+            config = json.load(conf_f)
+    else:
+        config = {"recent_hosts": []}
+
+    if not options.hosts:
+        # [!] Saving user selection as list
+        options.hosts = [common.select_host(config["recent_hosts"])]
+
+    for host in options.hosts:
+        print(
+            colorama.Fore.CYAN + colorama.Style.BRIGHT +
+            f"=== Saving configuration for '{host}' ===" +
+            colorama.Style.RESET_ALL
+        )
+
+        bin_backup_dst = host + "_old.backup"
+        config_backup_dst = host + "_old.rsc"
+        if (pathlib.Path(dest_path) / config_backup_dst).is_file():
+            print("Old configuration exists. Creating an update")
+            bin_backup_dst = host + "_new.backup"
+            config_backup_dst = host + "_new.rsc"
+
+
+        ssh_client = common.ssh_connect(host, credentials)
+
+        bin_backup, config_backup = common.get_backup_file_names(ssh_client)
+
+        print(f"Creating '{bin_backup}'")
+        common.exec_ssh_command(ssh_client, "/system backup save name=" + bin_backup)
+
+        print(f"Creating '{config_backup}'")
+        common.exec_ssh_command(ssh_client, "/export terse file=" + config_backup)
+
+        print(f"Saving config to '{str(dest_path)}'")
+        os.makedirs(dest_path, exist_ok=True)
+
+        sftp_client = ssh_client.open_sftp()
+
+        print(f"Downloading '{bin_backup}' as '{bin_backup_dst}'")
+        sftp_client.get(bin_backup, str(pathlib.Path(dest_path) / bin_backup_dst))
+        print(f"Removing '{bin_backup}'")
+        sftp_client.remove(bin_backup)
+
+        print(f"Downloading '{config_backup}' as '{config_backup_dst}'")
+        sftp_client.get(config_backup, str(pathlib.Path(dest_path) / config_backup_dst))
+        print(f"Removing '{config_backup}'")
+        sftp_client.remove(config_backup)
+
+        sftp_client.close()
+
+        try:
+            config["recent_hosts"].pop(config["recent_hosts"].index(host))
+        except ValueError:
+            pass
+        config["recent_hosts"].insert(0, host)
+
+        print(
+            colorama.Fore.GREEN + colorama.Style.BRIGHT +
+            f"{host}: done" +
+            colorama.Style.RESET_ALL
+        )
+
+    with open(config_file_name, "w", encoding="utf-8") as conf_f:
+        json.dump(config, conf_f, ensure_ascii=False, indent=4)
+
+if __name__ == "__main__":
+    main()
